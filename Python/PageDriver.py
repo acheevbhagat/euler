ebfebf443728d90b027bf9121ed6763e048798ca
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-07-18 01:13:33 -0400

    added answers to problems

--- Python/PageDriver.py
+++ Python/PageDriver.py
@@ -6,18 +6,18 @@ startTime = time.clock()
 pg = Page1()
 lists = Tools.Tools()
 
-print("Problem 1: " + str(pg.mult3_5()))  # problem 1
-print("Problem 2: " + str(pg.evenFib()))  # problem 2
-print("Problem 3: " + str(pg.largestPrime()))  # problem 3
-print("Problem 4: " + str(pg.pal()))  # problem 4
-print("Problem 5: " + str(pg.smallestMult()))  # problem 5
-print("Problem 6: " + str(pg.diffSquares()))  # problem 6
-print("Problem 7: " + str(pg.numPrime()))  # probelm 7
-print("Problem 8: " + str(pg.productInSeries()))  # problem 8
-print("Problem 9: " + str(pg.pythTriplet()))  # problem 9
-print("Problem 10: " + str(pg.sumPrime()))  # problem 10
-print("Problem 11: " + str(pg.gridProd()))  # problem 11
-print("Problem 12: " + str(pg.triangularNumber()))  # problem 12
-print("Problem 13: " + str(pg.largeSum()))  # problem 13
+print("Problem 1: " + str(pg.mult3_5()))  # problem 1 - 233168
+print("Problem 2: " + str(pg.evenFib()))  # problem 2 - 4613732
+print("Problem 3: " + str(pg.largestPrime()))  # problem 3 - 6857
+print("Problem 4: " + str(pg.pal()))  # problem 4 - 906609
+print("Problem 5: " + str(pg.smallestMult()))  # problem 55 - 232792560
+print("Problem 6: " + str(pg.diffSquares()))  # problem 6 - 25164150
+print("Problem 7: " + str(pg.numPrime()))  # probelm 7 - 104743
+print("Problem 8: " + str(pg.productInSeries()))  # problem 8 - 23514624000
+print("Problem 9: " + str(pg.pythTriplet()))  # problem 9 - 31875000
+print("Problem 10: " + str(pg.sumPrime()))  # problem 10 - 142913828922
+print("Problem 11: " + str(pg.gridProd()))  # problem 11 - 70600674
+print("Problem 12: " + str(pg.triangularNumber()))  # problem 12 - 76576500
+print("Problem 13: " + str(pg.largeSum()))  # problem 13 - 2493530798
 
 print("Running time - %s seconds" % (time.clock() - startTime))
--- Rust/main.rs
+++ Rust/main.rs
@@ -22,16 +22,16 @@ fn main()
 
     let pg: Page = Page::new();
 
-    println!("Problem 1: {}", pg.mult3_5());
-    println!("Problem 2: {}", pg.evenFib());
-    println!("Problem 3: {}", pg.largestPrime());
-    println!("Problem 4: {}", pg.pal());
-    println!("Problem 5: {}", pg.smallestMult());
-    println!("Problem 6: {}", pg.diffsquares());
-    println!("Problem 7: {}", pg.numPrime());
-    println!("Problem 8: {}", pg.productInSeries());
-    println!("Problem 9: {}", pg.pythTriplet());
-    println!("Problem 10: {}", pg.sumPrime());
+    println!("Problem 1: {}", pg.mult3_5()); // problem 1 - 233168
+    println!("Problem 2: {}", pg.evenFib()); // problem 2 - 4613732
+    println!("Problem 3: {}", pg.largestPrime()); // problem 3 - 6857
+    println!("Problem 4: {}", pg.pal()); // problem 4 - 906609
+    println!("Problem 5: {}", pg.smallestMult()); // problem 5 - 232792560
+    println!("Problem 6: {}", pg.diffsquares()); // problem 6 - 25164150
+    println!("Problem 7: {}", pg.numPrime()); // problem 7 - 104743
+    println!("Problem 8: {}", pg.productInSeries()); // problem 8 - 23514624000
+    println!("Problem 9: {}", pg.pythTriplet()); // problem 9 - 31875000
+    println!("Problem 10: {}", pg.sumPrime()); // problem 10 - 142913828922
 
     let time: String = now.elapsed().as_secs().to_string() + "." + &(now.elapsed().subsec_nanos() as f64 / (1 as f64).powi(10)).to_string();
     println!("Running time - {} seconds", time);
